aea79e45c8038ef1344b62270aa85ce39ad02e00
Anthony Cowley <acowley@tremolo>
2011-12-23 03:09:23 -0500

    Added a mildly optimized version of the quasicrystal animation.

---
+++ examples/QuasiCrystalDisplayFast.hs
@@ -0,0 +1,43 @@
+-- Port of the quasicrystal animation code at:
+-- http://mainisusuallyafunction.blogspot.com/2011/10/quasicrystals-as-sums-of-waves-in-plane.html
+-- This is a more thoughtfully optimized version of
+-- QuasiCrystalDisplay.hs. The optimization strategies are to precompute
+-- sines and cosines of the chosen angles, then assume that we are
+-- working with exactly 7 angles in the kernel code which then
+-- performs vectorized calculations.
+import System.GPU.CLUtil
+import qualified Data.Vector.Storable as V
+import Data.Word (Word8)
+import Graphics.Gloss hiding (Vector, scale)
+import System.IO.Unsafe
+
+pixels :: Int
+pixels = 800
+
+scale :: Float
+scale = 128
+
+angles :: Int -> Vector Float
+angles n = V.map (* (pi / fromIntegral n)) $ V.enumFromN 0 n
+
+mkPicture :: Vector Word8 -> Picture
+mkPicture = flip (bitmapOfForeignPtr pixels pixels) False 
+          . (\(x,_,_) -> x)
+          . V.unsafeToForeignPtr
+
+main = do s <- ezInit CL_DEVICE_TYPE_ALL
+          k <- kernelFromFile s "QuasiCrystalRGBAFast.cl" "quasiCrystal"
+          let numPix = pixels * pixels
+              pixels' = fromIntegral pixels :: CInt
+              numAngles = 7
+              allAngles = angles numAngles
+              sines = V.snoc (V.map sin allAngles) 0
+              cosines = V.snoc (V.map cos allAngles) 0
+              frame :: Float -> Picture
+              frame phase = unsafePerformIO $
+                            mkPicture `fmap`
+                            runKernel s k pixels' scale phase
+                                      sines cosines
+                                      (Out (numPix*4)) (Work2D pixels pixels)
+              {-# NOINLINE frame #-}
+          animate (InWindow "Quasicrystal" (pixels,pixels) (0,0)) black frame
---
+++ examples/QuasiCrystalRGBAFast.cl
@@ -0,0 +1,34 @@
+float wrap(float);
+
+__kernel void quasiCrystal(int pixels, 
+                           float scale,
+                           float phase,
+                           __constant float *cosAngles,
+                           __constant float *sinAngles,
+                           __global uchar *img) {
+  int x = get_global_id(0);
+  int y = get_global_id(1);
+  float denom = (float)pixels - 1;
+  float u = scale * ((2 * (float)x / denom) - 1);
+  float v = scale * ((2 * (float)y / denom) - 1);
+
+  /* If we know there are only 7 angles, then the host can pad
+   * precomputed sine and cosine vectors with an extra 0 and we can
+   * operate on float8 vectors. */
+
+  float8 cosines = vload8(0, cosAngles);
+  float8 sines = vload8(0, sinAngles);
+ 
+  float8 waves = (cos(cosines*u + sines*v + phase) + 1.0f) / 2.0f;
+  float sum = waves.s0 + waves.s1 + waves.s2 + waves.s3 + waves.s4 + waves.s5 + waves.s6;
+  uchar r = (uchar)(255.0f * clamp(wrap(sum), 0.0f, 1.0f));
+  vstore4((uchar4)(255,r,128,r), y*pixels + x, img);
+}
+
+float wrap(float n) {
+  int k = (int)n;
+  if(n < 0) k -= 1;
+  float v = n - (float)k;
+  if(k%2 == 1) v = 1 - v;
+  return v;
+}
