09ae2f2b496bc538cd639c48259c34a5f6662182
Anthony Cowley <acowley@gmail.com>
2014-01-07 22:27:24 -0500

    Added more initialization options.
    Direct selection of GPU, CPU, or by a user-provided predicate.
    
    Added an imperative mutable-state-heavy interface for sharing state
    between multiple CL action executions. This provides a way to share
    program parse state and allocated resource cleanup actions across an IO
    action. For example, within main, you may want to run OpenCL actions at
    several distinct points. This provides a way of sharing state between
    those execution points.

--- src/Control/Parallel/CLUtil/Initialization.hs
+++ src/Control/Parallel/CLUtil/Initialization.hs
@@ -3,17 +3,40 @@ module Control.Parallel.CLUtil.Initialization where
 import Control.Monad (void)
 import Control.Parallel.OpenCL
 import Control.Parallel.CLUtil.State
+import Data.Traversable (traverse)
 import Foreign.Ptr (nullPtr)
+
+clStateInit :: CLDeviceID -> IO OpenCLState
+clStateInit dev =
+  do context <- clCreateContext [] [dev] putStrLn
+     OpenCLState dev context `fmap` clCreateCommandQueue context dev []
 
 -- |Initialize the first device of the given type.
 ezInit :: CLDeviceType -> IO OpenCLState
 ezInit t = do (dev:_) <- clGetDeviceIDs nullPtr t
-              context <- clCreateContext [] [dev] putStrLn
-              q <- clCreateCommandQueue context dev []
-              return $ OpenCLState dev context q
+              clStateInit dev
 
 -- |Release a context and command queue.
 ezRelease :: OpenCLState -> IO ()
 ezRelease (OpenCLState _ c q) = 
   void $ clReleaseContext c >> clReleaseCommandQueue q
 
+-- |Initialize the first GPU device found.
+clDeviceGPU :: IO OpenCLState
+clDeviceGPU = ezInit CL_DEVICE_TYPE_GPU
+
+-- |Initialize the first CPU device found.
+clDeviceCPU :: IO OpenCLState
+clDeviceCPU = ezInit CL_DEVICE_TYPE_CPU
+
+-- |Initialize a device by selecting the first device of the given
+-- type that passes the given predicate. This may be used to
+-- distinguish between devices of the same type.
+clDeviceSelect :: CLDeviceType -> (CLDeviceID -> IO Bool)
+               -> IO (Maybe OpenCLState)
+clDeviceSelect t f = clGetDeviceIDs nullPtr t
+                     >>= firstM >>= traverse clStateInit 
+  where firstM [] = return Nothing
+        firstM (x:xs) = f x >>= \q -> case q of
+                          True -> return $ Just x
+                          False -> firstM xs
